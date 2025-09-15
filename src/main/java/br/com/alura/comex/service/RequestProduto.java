540f7f9b3669d6bb49d66c31a49d23a8181c0223
Eduardo Quinalha <equinalha@mppr.mp.br>
2025-09-15 17:50:53 -0300

    Ajuste nas validações

--- src/main/java/br/com/alura/comex/service/RequestProduto.java
+++ src/main/java/br/com/alura/comex/service/RequestProduto.java
@@ -7,20 +7,18 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public class RequestProduto {
-    @NotNull
+    @NotNull(message = "O nome não pode ser nulo")
     @NotBlank(message = "O nome não pode estar em branco.")
     @Size(min = 2, message = "O nome deve ter no mínimo 2 caracteres.")
     private String nome;
     
-    @NotNull
-    @NotBlank
+    @NotNull(message = "O preco não pode ser nulo")
     @PositiveOrZero(message = "O preço não pode ser negativo.")
     private Double preco;
 
     private String descricao;
     
-    @NotNull
-    @NotBlank
+    @NotNull(message = "A quantidade não pode ser nulo")
     private Long quantidade;
 
     public Produto toProduto() {
@@ -31,5 +29,37 @@ public class RequestProduto {
         produto.setQuantidade(this.quantidade);
 
         return produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
     }
 }
